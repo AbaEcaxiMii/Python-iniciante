cfa4e3f5fac8eb3a6413769332445658c89690e7
AbaEcaxiMii <andreymrigo@gmail.com>
2026-02-18 21:58:04 -0300

    treino

--- dia_2.py
+++ dia_2.py
@@ -1,8 +1,78 @@
 ##1005
-print("Hello World")
+a=float(input())
+b=float(input())
+media=(a*3.5+b*7.5)/11
+print(f"MEDIA = {media:.5f}")
 ##1006
+a=float(input())
+b=float(input())
+c=float(input())
+media=(a*2+b*3+c*5)/10
+print(f"MEDIA = {media:.1f}")
 ##1007
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+dif=(a*b-c*d)
+print(f"DIFERENCA = {dif}")
 ##1008
+number=int(input())
+hora=int(input())
+porhora=float(input())
+salary= hora*porhora
+print(f"NUMBER = {number}")
+print(f"SALARY = U$ {salary:.2f}")
 ##1009
+nome=input()
+sala_fixo=float(input())
+vendas=float(input())
+total=sala_fixo+(vendas*0.15)
+print(f"TOTAL = R$ {total:.2f}")
 ##1010
+cod1, quant1, valor1 = input().split()
+cod1 = int(cod1)
+quant1 = int(quant1)
+valor1 = float(valor1)
+
+cod2, quant2, valor2 = input().split()
+cod2 = int(cod2)
+quant2 = int(quant2)
+valor2 = float(valor2)
+
+total= (quant1*valor1)+(quant2*valor2)
+print(f"VALOR A PAGAR: R$ {total:.2f}")
 ##1011
+pi=3.14159
+raio=float(input())
+vol=(4/3)*pi*raio**3
+print(f"VOLUME = {vol:.3f}")
+##1012
+##1013
+##1014
+##1015
+##1016
+##1017
+##1018
+##1019
+##1020
+##1021
+##1022
+##1023
+##1024
+##1025
+##1026
+##1027
+##1028
+##1029
+##1030
+##1031
+##1032
+##1033
+##1034
+##1035
+##1036
+##1037
+##1038
+##1039
+##1040
---
+++ teste.py
@@ -0,0 +1,4 @@
+cod1, quant1, valor1 = input().split()
+cod1 = int(cod1)
+quant1 = int(quant1)
+valor1 = float(valor1)
