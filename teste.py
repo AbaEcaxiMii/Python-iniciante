ea45ebabca58d7ab1eaaac3155535aee9e128a5a
AbaEcaxiMii <andreymrigo@gmail.com>
2026-02-27 15:30:05 -0300

    a

--- teste.py
+++ teste.py
@@ -3,24 +3,33 @@ def limpar():
     os.system('cls' if os.name=='nt' else 'clear')
 limpar()
 
-salario = float(input())
+n=int(input())
+total=0
+total_c=0
+total_r=0
+total_s=0
+for i in range(n):
+    x = input().split()
+    a = int(x[0])
+    b = x[1]
+    
+    total+=a
+    
+    if b=="C":
+        total_c+=a
+    elif b=="R":
+        total_r+=a
+    elif b=="S":
+        total_s+=a
 
-if salario <= 2000:
-    print("Isento")
-else:
-    imposto = 0
-    if salario > 2000:
-        faixa8 = min(salario, 3000) - 2000.00 #resto, exemplo: mim(2002,3000) -> 2002-2000=2
-        if faixa8 > 0: #Não tem como cobrar imposto se não tem resto
-            imposto += faixa8 * 0.08
-    
-    if salario > 3000:
-        faixa18 = min(salario, 4500) - 3000
-        if faixa18 > 0:
-            imposto += faixa18 * 0.18
-    
-    if salario > 4500:
-        faixa28 = salario - 4500
-        imposto += faixa28 * 0.28
-    
-    print(f"R$ {imposto:.2f}")
+por_c=(total_c/total)*100
+por_r=(total_r/total)*100
+por_s=(total_s/total)*100
+
+print(f"Total: {total} cobaias")
+print(f"Total de coelhos: {total_c}")
+print(f"Total de ratos: {total_r}")
+print(f"Total de sapos: {total_s}")
+print(f"Percentual de coelhos: {por_c}")
+print(f"Percentual de ratos: {por_r}")
+print(f"Percentual de sapos: {por_s}")
